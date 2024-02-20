ad5107f8e381b67782611d566c59267e660b8b3e
AJAY E <ajaye2901@gmail.com>
2024-02-20 17:31:35 +0530

    stored the count into dict

--- Assessment_1/25_tuple.py
+++ Assessment_1/25_tuple.py
@@ -1,4 +1,16 @@
+def occurance(t) :
+
+    occu={}
+
+    for i in t:
+        if i in occu:
+            occu[i]=occu[i]+1
+        else:
+            occu[i]=1
+    return occu
+    
+
+
 t=(1,2,3,3,1,4,5,6,5)
 
-for i in t:
-    print("count of ",i,"is ",t.count(i))
+print(occurance(t))
